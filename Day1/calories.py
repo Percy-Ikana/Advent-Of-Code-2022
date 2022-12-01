d7e7b18a1335c58735599bba4e1eb1c509ee791a
Caleb Dunnell <cdunnell@gigersgroup.com>
2022-12-01 10:44:40 -0600

    Day One cleanup

--- Day1/calories.py
+++ Day1/calories.py
@@ -1,16 +1,17 @@
 with open('Day1/input') as file:
-    lines = file.readlines()
+    foodList = file.readlines()
 
 elfCalorieList = [0]
-mostPackedElfNumber = 0
-for line in lines:
-    if line == "\n":
+for snack in foodList:
+    if snack == "\n":
         #this means that we are on the next elf
         elfCalorieList.append(0)
     else:
         #this means that we add the calories to the current elf
-        elfCalorieList[len(elfCalorieList)-1] += int(line)
+        elfCalorieList[len(elfCalorieList)-1] += int(snack)
 
+#Look at the max element in the list, add it to the cal list, and print for posterity, 
+#then EXTERMINATE it from the list, do agian 2 more times for top three
 TopThreeCal = 0
 for i in range(3):
     TopThreeCal += max(elfCalorieList)
