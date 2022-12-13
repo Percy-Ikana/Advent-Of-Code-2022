afb05e34930e5c6c3a9f690e71929bb22d4bd3cb
Caleb Dunnell <cdunnell@gigersgroup.com>
2022-12-13 10:29:46 -0600

    Day 13 cleanup

--- Day13/decode part1.py
+++ Day13/decode part1.py
@@ -5,16 +5,17 @@ import functools
 def inOrder(left, right):
     determineShorterList = lambda l, r: l if (len(l) < len(r)) else r
 
-    #if they are equal we actually dont care
+    #figure out which list is shorter
     shortestList = determineShorterList(left, right)
 
+    #we need a "I dont know" value, or we will fail lists that sould pass.
     isInOrder = 'undefined'
     ranOut = False
     for index in range(len(shortestList)):
         if not isInOrder:
-            break
+            return isInOrder
         if isinstance(left[index], int) and isinstance(right[index], int):
-            #Okay, this needs to check UNTIL left is smaller than right, then it stops
+            #Okay, this needs to check UNTIL left is smaller than right, then it stops. I initally missed this and was very confused.
             if left[index] < right[index]:
                 return True
             elif left[index] > right[index]:
@@ -37,8 +38,10 @@ def inOrder(left, right):
         return True
     elif len(left) > len(right):
         return False
-    return 'undefined'
+    else:
+        return 'undefined'
 
+#this is not needed, I could make the inOrder func directly return these values.
 def compare(left, right):
     equal = inOrder(left, right)
     if equal:
@@ -55,9 +58,7 @@ def main():
     start_time = time.time()
 
     #part 1
-    part1Lists = [x.split('\n') for x in input.split('\n\n')]
-    #This should be able to be one line, not sure why it broke. fix later
-    part1Lists = [[literal_eval(y),literal_eval(z)] for y, z in part1Lists]
+    part1Lists = [[literal_eval(y), literal_eval(z)] for x in input.split('\n\n') for y, z in [x.split('\n')]]
 
     inOrderPairs = []
     for left, right in part1Lists:
@@ -66,9 +67,7 @@ def main():
             inOrderPairs.append(part1Lists.index([left,right])+1)
 
     #now for part 2
-    part2List = [x for x in input.split('\n')]
-    #This should be able to be one line, not sure why it broke. fix later
-    part2List = [literal_eval(y) for y in part2List if y != '']
+    part2List = [literal_eval(x) for x in input.split('\n') if x != '']
     #add the decode keys to the list
     part2List.append([[2]])
     part2List.append([[6]])
